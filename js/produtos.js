0dab36c30395823827d5b8dac68a4c8858d427bc
Bevilhonda <marcelinyuri@gmail.com>
2026-05-20 21:00:13 -0300

    cria função mensagem carrinho

--- js/produtos.js
+++ js/produtos.js
@@ -15,10 +15,14 @@ async function carregarProdutos(){
         await resposta.json();
 
     const container =
-        document.getElementById("produtos-container");
+        document.getElementById(
+            "produtos-container"
+        );
 
     const titulo =
-        document.getElementById("titulo-categoria");
+        document.getElementById(
+            "titulo-categoria"
+        );
 
     titulo.innerText =
         categoria.replace("-", " ");
@@ -31,14 +35,15 @@ async function carregarProdutos(){
         card.classList.add("produto-card");
 
         card.innerHTML = `
-            <img src="${produto.imagem}"
-                 alt="${produto.nome}">
+            <img
+                src="${produto.imagem}"
+                alt="${produto.nome}"
+            >
 
             <h3>${produto.nome}</h3>
 
             <p>
-                R$
-                ${produto.preco.toFixed(2)}
+                R$ ${produto.preco.toFixed(2)}
             </p>
 
             <button>
@@ -46,9 +51,64 @@ async function carregarProdutos(){
             </button>
         `;
 
+        const botao =
+            card.querySelector("button");
+
+        botao.addEventListener(
+            "click",
+            () => adicionarCarrinho(produto)
+        );
+
         container.appendChild(card);
 
     });
+
+}
+
+function adicionarCarrinho(produto){
+
+    const carrinho =
+        JSON.parse(
+            localStorage.getItem("carrinho")
+        ) || [];
+
+    carrinho.push(produto);
+
+    localStorage.setItem(
+        "carrinho",
+        JSON.stringify(carrinho)
+    );
+
+    mostrarMensagemCarrinho();
+}
+
+function mostrarMensagemCarrinho(){
+
+    const mensagem =
+        document.createElement("div");
+
+    mensagem.classList.add(
+        "mensagem-carrinho"
+    );
+
+    mensagem.innerText =
+        "Adicionado ao carrinho";
+
+    document.body.appendChild(
+        mensagem
+    );
+
+    setTimeout(() => {
+
+        mensagem.classList.add("mostrar");
+
+    }, 100);
+
+    setTimeout(() => {
+
+        mensagem.remove();
+
+    }, 2500);
 
 }
 
